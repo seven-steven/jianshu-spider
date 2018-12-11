df7ee466f29781b82c73b8f2d3351e50aba32c4d
Seven <seven@diqigan.cn>
2018-12-11 17:59:28 +0800

    初步实现文章爬取

--- src/model/jianshu.py
+++ src/model/jianshu.py
@@ -3,9 +3,11 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import html2text
 
 # 简书主页
-jianshu_root = "https://www.jianshu.com/"
+jianshu_root_url = "https://www.jianshu.com/"
+jianshu_post_url = jianshu_root_url + "p/"
 headers = {
     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
     "accept-encoding": "gzip, deflate, br",
@@ -17,48 +19,84 @@ headers = {
 }
 
 
-def get_post(url=None, post_id=None):
+def get_post(url=None, post_slug=None):
     # 设置文章 url
     if url is None:
-        if post_id is None:
+        if post_slug is None:
             # TODO 返回值
             return 1
         else:
-            url = jianshu_root + r"p/" + post_id
+            url = jianshu_post_url + post_slug
     # 获取网页源码
-    # html = requests.get(url=url, headers=headers)
+    html = requests.get(url=url, headers=headers)
     # 使用 BeautifulSoup 解析网页
-    # soup = BeautifulSoup(html.text, 'lxml')
-    soup = BeautifulSoup(open('post.html'), 'lxml')
+    soup = BeautifulSoup(html.text, 'lxml')
+    # soup = BeautifulSoup(open('post.html'), 'lxml')
 
+    # 记录文章信息
     post_message = {}
     # 获取文章标题
-    title = soup.title.string
-    # 获取作者信息
-    author = soup.select('.name a')
-    # 获取作者名称
-    author_name = author[0].text
+    title = soup.select('.article h1')[0].text
     # 获取作者 id
-    author_id = author[0].get('href')[3:]
+    author_slug = soup.select('.name a')[0].get('href')[3:]
     # 获取最后编辑时间
     publish_time = soup.select(".publish-time")[0].text
-    # 获取文章字数统计
-    word_count = soup.select(".wordage")[0].text[3:]
 
+    # 解析文章自带 json 信息
     message = soup.findAll('script', attrs={"data-name": "page-data", "type": "application/json"})
     message_json = json.loads(message[0].text)
-    # print(message_json)
-    print(message[0].text)
-    # print(message_json['locale'])
 
-
+    # 文章标题
     post_message['title'] = title
-    post_message['author_name'] = author_name
-    post_message['author_id'] = author_id
+    # 作者昵称
+    post_message['author_name'] = message_json['note']['author']['nickname']
+    # 作者 id
+    post_message['author_id'] = message_json['note']['user_id']
+    # 作者 slug
+    post_message['author_slug'] = author_slug
+    # 文章最后编辑时间
     post_message['publish_time'] = publish_time
-    post_message['word_count'] = word_count
-    # print(post_message)
+    # 文章字数统计
+    post_message['word_count'] = message_json['note']['public_wordage']
+    # 文章阅读数量统计
+    post_message['views_count'] = message_json['note']['views_count']
+    # 文章所属笔记本 id
+    post_message['notebook_id'] = message_json['note']['notebook_id']
+    # 文章 id
+    post_message['post_id'] = message_json['note']['id']
+    # 文章 slug
+    post_message['post_slug'] = message_json['note']['slug']
+    # 文章喜欢数量统计
+    post_message['likes_count'] = message_json['note']['likes_count']
+    # 文章是否可评论
+    post_message['commentable'] = message_json['note']['commentable']
+    # 文章评论数量
+    post_message['comments_count'] = message_json['note']['comments_count']
+    # 文章内容
+    content = soup.select('.show-content')[0]
+    # 提取图片并获取真实链接
+    imgs = content.findAll('img')
+    for img in imgs:
+        # 先删除可能存在的 src 属性
+        img['src'] = "https:" + img['data-original-src']
+
+    # 获取并删除图题
+    # TODO 可以尝试把图题转换为 alt 文本
+    img_captions = content.select('.image-caption')
+    [img_caption.extract() for img_caption in img_captions]
+
+    html2markdown = html2text.HTML2Text()
+    markdown = html2markdown.handle(str(content))
+    # 修复 html2text 错误换行
+    # TODO 这里需要优化, 只针对 a 标签和图片标签换行即可
+    markdown = markdown.replace('-\n', '-')
+
+    print("============== 文章信息 ==============")
+    print(post_message)
+
+    print("============== 文章内容 ==============")
+    print(markdown)
 
 
 if __name__ == "__main__":
-    get_post(post_id='6f1e68d1b7a1')
+    get_post(post_slug='4e59453b602b')
